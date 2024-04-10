440e38080e46bb43be7862167b45b1998b3920e3
marya <you@example.com>
2024-04-10 21:18:56 +0300

    свайпер

--- src/js/index.js
+++ src/js/index.js
@@ -15,3 +15,41 @@ const brandswiper = new Swiper('.swiper', {
   loop: true,
   direction: 'horizontal'
 })
+
+
+
+
+
+
+
+
+
+const textHidable = document.querySelector('.about__text--hidable');
+const readMore = document.querySelectorAll('.read-more');
+const readMoreText = document.querySelectorAll('read-more__text'); 
+
+const desctop = document.querySelector('.desctop__content');
+const desctopRepair = document.querySelector('.desctop__content--repair');
+
+readMore[0].addEventListener('click', function () {
+    if (textHidable.classList.contains('about__text--hidable')) {
+      textHidable.classList.remove('about__text--hidable')
+      readMore[0].getElementsByClassName('read-more__text')[0].innerHTML = 'Скрыть'
+    } else {
+      textHidable.classList.add('about__text--hidable')
+      readMore[0].getElementsByClassName('read-more__text')[0].innerHTML = 'Читать все';
+    }
+  })
+  
+  
+  
+  readMore[1].addEventListener('click',function(){
+    desctop.classList.toggle('desctop__content--click');
+   
+    if(desctop.classList.contains('desctop__content--click')){
+      readMore[1].getElementsByClassName('read-more__text')[0].innerHTML = 'Скрыть все';
+    } else{
+      readMore[1].getElementsByClassName('read-more__text')[0].innerHTML = 'Показать все';
+    }
+  
+  })
