887c4b101f52d0098c5f6f3bbcece98c05b76de3
marya <you@example.com>
2024-04-10 19:54:53 +0300

    Первый свайпер

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 import '../scss/style.scss'
+import Swiper, { Navigation, Pagination } from 'swiper'
+import { doc } from 'prettier'
+Swiper.use([Navigation, Pagination])
+
+
+const brandswiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination'
+  },
+
+  slidesPerView: 'auto',
+
+  spaceBetween: 16,
+  loop: true,
+  direction: 'horizontal'
+})
